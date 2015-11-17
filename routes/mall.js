a1c3fd2bfef7a3c967fb961e4c812715595ea8f5
Racomaizer <alemonmk@gmail.com>
2015-11-17 21:50:22 +0800

    Add a new route to enable query of single mall data

--- routes/mall.js
+++ routes/mall.js
@@ -15,24 +15,38 @@ module.exports = function (database) {
 		res.status(401).json({status: 401, message: msg.ERR_NOTAUTHED});
 	}
 
-	mallRouter.route('/')
-		.get(checkAuth, (req, res) => {
-			if (req.query.long && req.query.lat) {
-				MallModel
-				/*.geoNear(
-				 { type: "Point", coordinates: [req.query.long, req.query.lat] },
-				 { maxDistance: geoDistLimit, spherical: true }
-				 ).then(*/
-					.findOne().exec(
-					(err, m, stats) => {
-						if (!m) res.status(404).json({status: 404, message: msg.ERR_NOMALLNEARBY});
-						res.status(200).json(m);
-					}
-				);
-			} else {
-				res.status(400).json({status: 400, message: msg.ERR_INVALIDLOC})
-			}
-		});
+	mallRouter.get('/', checkAuth, (req, res) => {
+		if (req.query.long && req.query.lat) {
+			MallModel
+			/*.geoNear(
+			 { type: "Point", coordinates: [req.query.long, req.query.lat] },
+			 { maxDistance: geoDistLimit, spherical: true }
+			 ).then(*/
+				.findOne().exec(
+				(err, m, stats) => {
+					if (!m) res.status(404).json({status: 404, message: msg.ERR_NOMALLNEARBY});
+					res.status(200).json(m);
+				}
+			);
+		} else {
+			res.status(400).json({status: 400, message: msg.ERR_INVALIDLOC})
+		}
+	});
+
+	mallRouter.get('/:id', checkAuth, (req, res) => {
+		MallModel
+		/*.geoNear(
+		 { type: "Point", coordinates: [req.query.long, req.query.lat] },
+		 { maxDistance: geoDistLimit, spherical: true }
+		 ).then(*/
+				.findById(req.params.id).exec(
+				(err, m, stats) => {
+					if (!m) res.status(404).json({status: 404, message: msg.ERR_NOMALLNEARBY});
+					res.status(200).json(m);
+				}
+		);
+	});
+
 
 	return mallRouter;
 };
